14ab9af90c356a093b251dda38148fea486ef727
escariche <javiescariz.je@gmail.com>
2018-05-08 12:28:52 +0200

    Server.properties updated in launcVC.py

--- kafka/launch_VC.py
+++ kafka/launch_VC.py
@@ -5,6 +5,6 @@ import os
 os.system("kafka_ZK/bin/zookeeper-server-start.sh kafka_ZK/config/zookeeper.properties &")
 
 #DataNode0
-os.system("kafka_N0/bin/kafka-server-start.sh kafka_N0/config/server.properties &")
+os.system("kafka_N0/bin/kafka-server-start.sh kafka_N0/config/server_0.properties &")
 #DataNode1
-os.system("kafka_N1/bin/kafka-server-start.sh kafka_N1/config/server.properties &")
+os.system("kafka_N1/bin/kafka-server-start.sh kafka_N1/config/server_1.properties &")
