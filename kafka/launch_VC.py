e324662c2ad9f84dca3a2acf1304f8f4fc9c15f0
escariche <javiescariz.je@gmail.com>
2018-05-10 18:17:26 +0200

    unified folder for kafka. new launch.py

--- kafka/launch_VC.py
+++ kafka/launch_VC.py
@@ -1,10 +1,12 @@
 import os
 
+dir = "kafka_unified_settings/"
 #Starting kafka nodes
 #Zookeeper
-os.system("kafka_ZK/bin/zookeeper-server-start.sh kafka_ZK/config/zookeeper.properties &")
+os.system( dir + "bin/zookeeper-server-start.sh " + dir +  "config/zookeeper.properties &")
+#os.system("kafka_ZK/bin/zookeeper-server-start.sh kafka_ZK/config/zookeeper.properties &")
 
 #DataNode0
-os.system("kafka_N0/bin/kafka-server-start.sh kafka_N0/config/server_0.properties &")
+os.system(dir + "bin/kafka-server-start.sh " + dir +  "config/server_0.properties &")
 #DataNode1
-os.system("kafka_N1/bin/kafka-server-start.sh kafka_N1/config/server_1.properties &")
+os.system(dir + "bin/kafka-server-start.sh " + dir +  "config/server_1.properties &")
